4020dac4ab30c949d9218db215e5621e6592b610
Apriorhythm <Apriorhythm@gmail.com>
2017-04-23 06:31:59 +0800

    add field logo to user model

--- outis_user/models.py
+++ outis_user/models.py
@@ -4,20 +4,23 @@
 否则会出现乱码或服务器无法启动等奇怪现象
 """
 
-
 from __future__ import unicode_literals
 
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
 
 class OutisUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     heat = models.SmallIntegerField('heat', default=1)
+    logo = models.ImageField()
+
 
     def __str__(self):
         return self.username
 
     class Meta:
-        verbose_name = 'Outis User'
-        verbose_name_plural = 'Outis User'
+        verbose_name = 'Outis_User'
+        verbose_name_plural = 'Outis_User'
 
